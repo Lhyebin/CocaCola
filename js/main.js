efe6b7c1a21c7ca0ee13201ea35e2618e7f6a2a0
Lhyebin <133613625+Lhyebin@users.noreply.github.com>
2023-08-24 17:51:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 $(function () {
+    //@media : header 메뉴바
+    $('.mopen').on('click', function () {
+        $('.top_bar').toggleClass('on')
+    })
+
+    $('.top_bar>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().slideToggle();
+        $(this).parent().siblings().find('.sub').slideUp();
+        // -> 마지막줄 반응형에서 다른 서브메뉴 클릭했을때 이전 서브메뉴 들어가게하기
+    })
+
+
+
+
+
+
+
 
     // 메인비쥬얼 다음페이지
     $('.main_slide').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2500,
         speed: 1000,
         pauseOnHover: false,
         pauseOnFocus: false,
@@ -33,10 +51,18 @@ $(function () {
     $('.product').slick({
         slidesToShow: 5,
         autoplay: true,
-        autoplaySpeed: 1700,
+        autoplaySpeed: 2000,
         speed: 1000,
         arrows: false,
         centerMode: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 
     $('.product_wrap .dots li').on('click', function () {
